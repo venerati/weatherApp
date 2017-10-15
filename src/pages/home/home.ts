866e1e2a12bb421a931b957c4997dbea370480b2
James Marshall <jamesomarshall@gmail.com>
2017-10-15 15:34:13 -0400

    changed the ui to have the current temp dominate the page, details will be displayed lower. removed the "now" card. created a refresh button and functions to tie it all in. It all works as desired. created function to scrub the desimal point from the current temp.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,6 +21,7 @@ export class HomePage {
 		lat:any,
 		long:any,
 	}
+	mainTemp:any;
 
   constructor(
   	public navCtrl:NavController, 
@@ -34,8 +35,83 @@ export class HomePage {
   		this.navCtrl.push(SettingsPage);
   	}
 
+	mainTempFunc(){
+		this.mainTemp = this.weather.temp_f.toFixed(0);
+		console.log("maintempfunc has fired");
+		console.log(this.mainTemp);
+	}
+
+	refresh(){
+		console.log("refresh has fired")
+				//This grabs the value of 'location' for local storage and hands it to 'val'
+		this.storage.get('location').then((val) =>{
+			if (val !== null) {
+				//this takes the val and turns it into a json string
+				this.location = JSON.parse(val);
+				console.log(val);
+
+				//if statement to determin if the user has put in a zip or a city
+				if (this.location.city != null) {
+					console.log('city api has fired')
+					//this calls the 'getWeather' function and passes the city and state to it. It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
+				  	this.WeatherApi.getWeather(this.location.city, this.location.state).subscribe(weather => {
+				  		this.weather = weather.current_observation;
+				  	});
+
+				  	console.log(this.weather);
+
+				  	this.WeatherApi.getWeatherForecast(this.location.city, this.location.state).subscribe(weatherForecast => {
+				  		this.weatherForecast = weatherForecast.forecast;
+				  	});
+
+				  	this.mainTempFunc();
+
+				  	console.log(this.weatherForecast);
+				//checks to see if zip holds a value, if it does then this fires, if not then gps will fire
+				} else if (this.location.zip != null) {
+					console.log('the zip api has fired')
+					//this calls the 'getWeatherZip' funtion and passes the zip into the api call.It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
+					this.WeatherApi.getWeatherZip(this.location.zip).subscribe(weather => {
+				  		this.weather = weather.current_observation;
+				  	});
+
+				  	this.WeatherApi.getWeatherForecastZip(this.location.zip).subscribe(weatherForecast => {
+				  		this.weatherForecast = weatherForecast.forecast;
+				  	});
+
+				  	this.mainTempFunc();
+
+				  	console.log(this.weatherForecast);
+				} else {
+					console.log('the gps api has fired')
+					console.log(this.location.lat, this.location.long)
+					//this calls the 'getWeatherZip' funtion and passes the zip into the api call.It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
+					this.WeatherApi.getWeatherGPS(this.location.lat, this.location.long).subscribe(weather => {
+				  		this.weather = weather.current_observation;
+				  	});
+
+				  	this.WeatherApi.getWeatherForecastGPS(this.location.lat, this.location.long).subscribe(weatherForecast => {
+				  		this.weatherForecast = weatherForecast.forecast;
+				  	});
+
+				  	this.mainTempFunc();
+				  
+				  	console.log(this.weatherForecast);
+
+				}
+
+			} else {
+				//bring the settings page up for the user to put in location
+				this.navCtrl.push(SettingsPage);
+			}
+		});
+
+	}
+
     //this will fire when the component is loaded
 	ionViewWillEnter(){
+
+
 
 
 		//This grabs the value of 'location' for local storage and hands it to 'val'
@@ -59,6 +135,8 @@ export class HomePage {
 				  		this.weatherForecast = weatherForecast.forecast;
 				  	});
 
+				  	this.mainTempFunc();
+
 				  	console.log(this.weatherForecast);
 				//checks to see if zip holds a value, if it does then this fires, if not then gps will fire
 				} else if (this.location.zip != null) {
@@ -72,6 +150,8 @@ export class HomePage {
 				  		this.weatherForecast = weatherForecast.forecast;
 				  	});
 
+				  	this.mainTempFunc();
+
 				  	console.log(this.weatherForecast);
 				} else {
 					console.log('the gps api has fired')
@@ -84,6 +164,8 @@ export class HomePage {
 				  	this.WeatherApi.getWeatherForecastGPS(this.location.lat, this.location.long).subscribe(weatherForecast => {
 				  		this.weatherForecast = weatherForecast.forecast;
 				  	});
+
+				  	this.mainTempFunc();
 
 				  	console.log(this.weatherForecast);
 
