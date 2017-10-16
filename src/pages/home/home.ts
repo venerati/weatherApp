e507e50c41b757849b70448d4684f43e3cdb2be5
James Marshall <jamesomarshall@gmail.com>
2017-10-16 18:53:46 -0400

    created a parent function for the storage call on page load for home.ts. started to move UI around but it still needs work.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -30,6 +30,72 @@ export class HomePage {
   	) {
 
   }
+
+  	dataCall(){
+  		//This grabs the value of 'location' for local storage and hands it to 'val'
+		this.storage.get('location').then((val) =>{
+			if (val !== null) {
+				//this takes the val and turns it into a json string
+				this.location = JSON.parse(val);
+				console.log(val);
+
+				//if statement to determin if the user has put in a zip or a city
+				if (this.location.city != null) {
+					console.log('city api has fired')
+					//this calls the 'getWeather' function and passes the city and state to it. It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
+				  	this.WeatherApi.getWeather(this.location.city, this.location.state).subscribe(weather => {
+				  		this.weather = weather.current_observation;
+				  	});
+
+				  	console.log(this.weather);
+
+				  	this.WeatherApi.getWeatherForecast(this.location.city, this.location.state).subscribe(weatherForecast => {
+				  		this.weatherForecast = weatherForecast.forecast;
+				  	});
+
+				  	this.mainTempFunc();
+
+				  	console.log(this.weatherForecast);
+				//checks to see if zip holds a value, if it does then this fires, if not then gps will fire
+				} else if (this.location.zip != null) {
+					console.log('the zip api has fired')
+					//this calls the 'getWeatherZip' funtion and passes the zip into the api call.It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
+					this.WeatherApi.getWeatherZip(this.location.zip).subscribe(weather => {
+				  		this.weather = weather.current_observation;
+				  	});
+
+				  	this.WeatherApi.getWeatherForecastZip(this.location.zip).subscribe(weatherForecast => {
+				  		this.weatherForecast = weatherForecast.forecast;
+				  	});
+
+				  	this.mainTempFunc();
+
+				  	console.log(this.weatherForecast);
+				} else {
+					console.log('the gps api has fired')
+					console.log(this.location.lat, this.location.long)
+					//this calls the 'getWeatherZip' funtion and passes the zip into the api call.It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
+					this.WeatherApi.getWeatherGPS(this.location.lat, this.location.long).subscribe(weather => {
+				  		this.weather = weather.current_observation;
+				  	});
+
+				  	this.WeatherApi.getWeatherForecastGPS(this.location.lat, this.location.long).subscribe(weatherForecast => {
+				  		this.weatherForecast = weatherForecast.forecast;
+				  	});
+
+				  	this.mainTempFunc();
+
+				  	console.log(this.weatherForecast);
+
+				}
+
+			} else {
+				//bring the settings page up for the user to put in location
+				this.navCtrl.push(SettingsPage);
+			}
+		});
+  	}
+
   	// this function is called when the user clicks on the serch icon. It brings the search page up.
   	search(){
   		this.navCtrl.push(SettingsPage);
@@ -43,138 +109,15 @@ export class HomePage {
 
 	refresh(){
 		console.log("refresh has fired")
-				//This grabs the value of 'location' for local storage and hands it to 'val'
-		this.storage.get('location').then((val) =>{
-			if (val !== null) {
-				//this takes the val and turns it into a json string
-				this.location = JSON.parse(val);
-				console.log(val);
-
-				//if statement to determin if the user has put in a zip or a city
-				if (this.location.city != null) {
-					console.log('city api has fired')
-					//this calls the 'getWeather' function and passes the city and state to it. It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
-				  	this.WeatherApi.getWeather(this.location.city, this.location.state).subscribe(weather => {
-				  		this.weather = weather.current_observation;
-				  	});
-
-				  	console.log(this.weather);
-
-				  	this.WeatherApi.getWeatherForecast(this.location.city, this.location.state).subscribe(weatherForecast => {
-				  		this.weatherForecast = weatherForecast.forecast;
-				  	});
-
-				  	this.mainTempFunc();
-
-				  	console.log(this.weatherForecast);
-				//checks to see if zip holds a value, if it does then this fires, if not then gps will fire
-				} else if (this.location.zip != null) {
-					console.log('the zip api has fired')
-					//this calls the 'getWeatherZip' funtion and passes the zip into the api call.It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
-					this.WeatherApi.getWeatherZip(this.location.zip).subscribe(weather => {
-				  		this.weather = weather.current_observation;
-				  	});
-
-				  	this.WeatherApi.getWeatherForecastZip(this.location.zip).subscribe(weatherForecast => {
-				  		this.weatherForecast = weatherForecast.forecast;
-				  	});
-
-				  	this.mainTempFunc();
-
-				  	console.log(this.weatherForecast);
-				} else {
-					console.log('the gps api has fired')
-					console.log(this.location.lat, this.location.long)
-					//this calls the 'getWeatherZip' funtion and passes the zip into the api call.It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
-					this.WeatherApi.getWeatherGPS(this.location.lat, this.location.long).subscribe(weather => {
-				  		this.weather = weather.current_observation;
-				  	});
-
-				  	this.WeatherApi.getWeatherForecastGPS(this.location.lat, this.location.long).subscribe(weatherForecast => {
-				  		this.weatherForecast = weatherForecast.forecast;
-				  	});
-
-				  	this.mainTempFunc();
-				  
-				  	console.log(this.weatherForecast);
-
-				}
-
-			} else {
-				//bring the settings page up for the user to put in location
-				this.navCtrl.push(SettingsPage);
-			}
-		});
-
+		this.dataCall()
 	}
 
     //this will fire when the component is loaded
 	ionViewWillEnter(){
 
+		this.dataCall()
 
 
-
-		//This grabs the value of 'location' for local storage and hands it to 'val'
-		this.storage.get('location').then((val) =>{
-			if (val !== null) {
-				//this takes the val and turns it into a json string
-				this.location = JSON.parse(val);
-				console.log(val);
-
-				//if statement to determin if the user has put in a zip or a city
-				if (this.location.city != null) {
-					console.log('city api has fired')
-					//this calls the 'getWeather' function and passes the city and state to it. It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
-				  	this.WeatherApi.getWeather(this.location.city, this.location.state).subscribe(weather => {
-				  		this.weather = weather.current_observation;
-				  	});
-
-				  	console.log(this.weather);
-
-				  	this.WeatherApi.getWeatherForecast(this.location.city, this.location.state).subscribe(weatherForecast => {
-				  		this.weatherForecast = weatherForecast.forecast;
-				  	});
-
-				  	this.mainTempFunc();
-
-				  	console.log(this.weatherForecast);
-				//checks to see if zip holds a value, if it does then this fires, if not then gps will fire
-				} else if (this.location.zip != null) {
-					console.log('the zip api has fired')
-					//this calls the 'getWeatherZip' funtion and passes the zip into the api call.It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
-					this.WeatherApi.getWeatherZip(this.location.zip).subscribe(weather => {
-				  		this.weather = weather.current_observation;
-				  	});
-
-				  	this.WeatherApi.getWeatherForecastZip(this.location.zip).subscribe(weatherForecast => {
-				  		this.weatherForecast = weatherForecast.forecast;
-				  	});
-
-				  	this.mainTempFunc();
-
-				  	console.log(this.weatherForecast);
-				} else {
-					console.log('the gps api has fired')
-					console.log(this.location.lat, this.location.long)
-					//this calls the 'getWeatherZip' funtion and passes the zip into the api call.It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
-					this.WeatherApi.getWeatherGPS(this.location.lat, this.location.long).subscribe(weather => {
-				  		this.weather = weather.current_observation;
-				  	});
-
-				  	this.WeatherApi.getWeatherForecastGPS(this.location.lat, this.location.long).subscribe(weatherForecast => {
-				  		this.weatherForecast = weatherForecast.forecast;
-				  	});
-
-				  	this.mainTempFunc();
-
-				  	console.log(this.weatherForecast);
-
-				}
-
-			} else {
-				//bring the settings page up for the user to put in location
-				this.navCtrl.push(SettingsPage);
-			}
-		});
+		
   	}
 }
