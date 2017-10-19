9e2d61c4f67b8bfdd0835dae2e8197114c5a11cd
James Marshall <jamesomarshall@gmail.com>
2017-10-19 15:08:21 -0400

    Fixed a bug were main temp would not display upon page load. This was due to the funtion that removed the decimal place from the temp value being out of scope of the original api call/binding. Once I moved the decimal function into the main data binding the value displayed as desired.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -31,6 +31,14 @@ export class HomePage {
 
   }
 
+    	// this function takes the current weather tempature and removes any decimal point
+	mainTempFunc(){
+
+		this.mainTemp = this.weather.temp_f.toFixed(0);
+		console.log("maintempfunc has fired");
+		console.log(this.mainTemp);
+	}
+
   	dataCall(){
   		//This grabs the value of 'location' for local storage and hands it to 'val'
 		this.storage.get('location').then((val) =>{
@@ -45,6 +53,7 @@ export class HomePage {
 					//this calls the 'getWeather' function and passes the city and state to it. It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
 				  	this.WeatherApi.getWeather(this.location.city, this.location.state).subscribe(weather => {
 				  		this.weather = weather.current_observation;
+				  		this.mainTempFunc();
 				  	});
 
 				  	console.log(this.weather);
@@ -53,8 +62,6 @@ export class HomePage {
 				  		this.weatherForecast = weatherForecast.forecast;
 				  	});
 
-				  	this.mainTempFunc();
-
 				  	console.log(this.weatherForecast);
 				//checks to see if zip holds a value, if it does then this fires, if not then gps will fire
 				} else if (this.location.zip != null) {
@@ -62,13 +69,12 @@ export class HomePage {
 					//this calls the 'getWeatherZip' funtion and passes the zip into the api call.It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
 					this.WeatherApi.getWeatherZip(this.location.zip).subscribe(weather => {
 				  		this.weather = weather.current_observation;
+				  		this.mainTempFunc();
 				  	});
 
 				  	this.WeatherApi.getWeatherForecastZip(this.location.zip).subscribe(weatherForecast => {
 				  		this.weatherForecast = weatherForecast.forecast;
 				  	});
-
-				  	this.mainTempFunc();
 
 				  	console.log(this.weatherForecast);
 				} else {
@@ -77,13 +83,12 @@ export class HomePage {
 					//this calls the 'getWeatherZip' funtion and passes the zip into the api call.It this subscribes to the observable and we call it weather. This returns the json object that the API hands back.
 					this.WeatherApi.getWeatherGPS(this.location.lat, this.location.long).subscribe(weather => {
 				  		this.weather = weather.current_observation;
+				  		this.mainTempFunc();
 				  	});
 
 				  	this.WeatherApi.getWeatherForecastGPS(this.location.lat, this.location.long).subscribe(weatherForecast => {
 				  		this.weatherForecast = weatherForecast.forecast;
 				  	});
-
-				  	this.mainTempFunc();
 
 				  	console.log(this.weatherForecast);
 
@@ -101,12 +106,8 @@ export class HomePage {
   		this.navCtrl.push(SettingsPage);
   	}
 
-	mainTempFunc(){
-		this.mainTemp = this.weather.temp_f.toFixed(0);
-		console.log("maintempfunc has fired");
-		console.log(this.mainTemp);
-	}
 
+	//this is called by a button on the home.html file and it updated the api call
 	refresh(){
 		console.log("refresh has fired")
 		this.dataCall()
@@ -116,8 +117,5 @@ export class HomePage {
 	ionViewWillEnter(){
 
 		this.dataCall()
-
-
-		
   	}
 }
